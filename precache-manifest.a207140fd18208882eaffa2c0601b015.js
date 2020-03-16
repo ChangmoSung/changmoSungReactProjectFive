32123d2168ce9f6d03de79c51e39cff6a7cf90c1
ChangmoSung <changmosung0914@gmail.com>
2020-03-16 11:07:19 -0400

    Updates

---
+++ precache-manifest.a207140fd18208882eaffa2c0601b015.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "296d957c508e78eb337156882a1f755d",
+    "url": "/changmoSungReactProjectFive/index.html"
+  },
+  {
+    "revision": "b16e65b5a7303e772591",
+    "url": "/changmoSungReactProjectFive/static/css/main.a909cbb4.chunk.css"
+  },
+  {
+    "revision": "15d93b32dc20033e1a3b",
+    "url": "/changmoSungReactProjectFive/static/js/2.43867b60.chunk.js"
+  },
+  {
+    "revision": "7ba5a91c6aa4ae9cc068a25fb13d169c",
+    "url": "/changmoSungReactProjectFive/static/js/2.43867b60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b16e65b5a7303e772591",
+    "url": "/changmoSungReactProjectFive/static/js/main.b7327bbc.chunk.js"
+  },
+  {
+    "revision": "ba99061140dad373e29c",
+    "url": "/changmoSungReactProjectFive/static/js/runtime-main.e3260376.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/changmoSungReactProjectFive/index.html/precache-manifest.72daad1af1cd7100cc95bbc79c2abd0d.js"
+  "/changmoSungReactProjectFive/precache-manifest.a207140fd18208882eaffa2c0601b015.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/changmoSungReactProjectFive/index.html/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/changmoSungReactProjectFive/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
